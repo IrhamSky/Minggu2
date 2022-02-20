9dde99617f5ea60afa05b840984a661598ce2d26
irhamsky <irham_21520026@stimata.ac.id>
2022-02-20 22:21:02 +0700

    Latihan array 1

---
+++ Latihan1.php
@@ -0,0 +1,15 @@
+<?php
+//Penulisan Array
+$hari = array("Senin", "Selasa", "Rabu");
+$bulan = ["Januari", "Februari", "Maret"];
+$gabungan = [100, "teks", true];
+
+//memanggil Array
+//Versi debugging atau versi programmer
+var_dump($hari);
+echo "<br>";
+print_r($bulan);
+echo "<br>";
+//versi user
+
+echo $gabungan[0];
